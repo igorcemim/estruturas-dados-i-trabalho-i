fa65dd3310a41c5028c0e5a6114d4b34f38e3bf8
Igor Cemim <igor@cemim.com.br>
2017-11-08 15:37:31 -0200

    Adiciondas instruções e modificado make

---
+++ README.txt
@@ -0,0 +1,8 @@
+Dar permissão de execução ao make.sh, executando o comando:
+  chmod +x make.sh
+
+Compilar o programa, executando o comando:
+  ./make.sh
+
+Executar o programa com o comando:
+  ./main
--- make.sh
+++ make.sh
@@ -1,1 +1,9 @@
-gcc -o main main.c lib/*.c
+NOME_EXECUTAVEL="main"
+
+# Se o arquivo existir remove
+if [ -f main ]; then
+    rm $NOME_EXECUTAVEL
+fi
+
+# Compila o programa
+gcc -o $NOME_EXECUTAVEL main.c lib/*.c
